6be9e4a933731ddd885dc7086e7f0377183e1b39
Ray Suyoto <ray.suy@icloud.com>
2018-03-12 10:15:27 -0600

    update routes

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,16 +1,44 @@
 class Api::ProductsController < ApplicationController
+  before_action :set_product, only: [:show, :update, :destroy]
+
   def index
+    render json: Product.all
   end
 
   def show
+    render json: @product
   end
 
   def create
+    product = Product.new(product_params)
+
+    if product.save
+      render json: product
+    else
+      render json: product.errors, status: 422
+    end
   end
 
   def update
+    if @product.update(product_params)
+      render json: @product
+    else
+      render json: @product.errors, status: 422
+    end
   end
 
   def destroy
+    @product.destroy
   end
+ 
+  private
+    def product_params
+      params.require(:product).permit(:name, :description, :department, :price)
+    end
+
+    def set_product
+      @product = Product.find(params[:id])
+    end
+
+
 end
