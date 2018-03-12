49e14e57f9601fd73796b7257b00c4c029c1c37b
Ray Suyoto <ray.suy@icloud.com>
2018-03-12 10:35:43 -0600

    define routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,9 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Route } from 'react-router-dom';
+import Home from './components/Home';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <Route exact path="/" component={Home} />
+)
 
 export default App;
